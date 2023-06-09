5fedc8a438c7be7b3a1a1cc87109dc01119fa270
Alejandro <ajorgenmarten35@gmail.com>
2023-06-09 16:52:39 -0400

    agregando mas valores al arbol :)

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import Arbol from "./arbol";
 
-const arbol = new Arbol([5, 2, 7, 1, 3, 6, 8])
+const arbol = new Arbol([5, 2, 7, 1, 3, 6, 8, 9, 4])
 
 console.log(arbol.inOrden())
 console.log(arbol.preOrden())
